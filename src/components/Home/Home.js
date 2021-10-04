2f3a021483f938d6d7997421931060d2fde94e48
Khurshida-Meem <meemrumi59@gmail.com>
2021-10-04 09:49:40 +0600

    added notes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Notes from '../Notes/Notes';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Notes></Notes>
         </div>
     );
 };
